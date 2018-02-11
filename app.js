c222319e3c60bcccf6769c36b0558b75b500fe5f
Dennis Tran <nartdensin@gmail.com>
2018-02-10 21:30:53 -0800

    added heroes route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var search = require('./routes/search');
+var heroes = require('./routes/heroes');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/search', search);
+app.use('/heroes', heroes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/heroes.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var url = 'mongodb://localhost:27017/';
+var mongo = require('mongodb').MongoClient
+var router = express.Router();
+
+/* GET search. */
+router.get('/', function(req, res, next) {
+    mongo.connect(url, function(err, db) {
+        const myDb = db.db('dota2matchup');
+        if (err) throw err;
+        myDb.collection('heroes', function(err, collection) {
+            if (err) throw err;
+            collection.find().sort(
+                { localized_name: 1 }
+            ).toArray(function(err, data) {
+                if (err) throw err;
+                res.send(data);
+                db.close();
+            })
+        });
+    })  
+});
+
+module.exports = router;
