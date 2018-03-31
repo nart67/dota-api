33bb4150740676b507965f73e7b6b89f1ee3e9d6
Dennis Tran <nartdensin@gmail.com>
2018-03-30 21:56:11 -0700

    update with database calls

--- helper/passport.js
+++ helper/passport.js
@@ -1,31 +1,27 @@
 var ExtractJwt = require('passport-jwt').ExtractJwt,
     JwtStrategy = require('passport-jwt').Strategy;
+var Users = require('../models/users');
 
 var jwtOptions = {};
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOptions.secretOrKey = process.env.SECRET; // Add secret in env
 
-var users = [
-    {
-      id: 1,
-      name: 'test',
-      password: 'test'
-    }
-  ];
-
 var strategy = new JwtStrategy(jwtOptions,
     function(jwt_payload, next) {
-    // Enter database call here
-    console.log('payload received', jwt_payload);
     
-    if (users[0].id === jwt_payload.id) {
-        next(null, users[0]);
-    } else {
-        next(null, false);
-    }
+    Users.findUserId(jwt_payload.id, function(err, user) {
+        if (err) {
+            console.log(err);
+            next(null, false);
+        }
+        else {
+            if (user.id === jwt_payload.id) {
+                next(null, user);
+            }
+        }
+    })
 });
 
 module.exports = {
-    strategy: strategy,
-    users: users
+    strategy: strategy
 };
--- routes/login.js
+++ routes/login.js
@@ -1,27 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var users = require('../helper/passport').users;
+var users = require('../models/users');
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 router.post("/", function (req, res) {
     if (req.body.name && req.body.password) {
         var name = req.body.name;
         var password = req.body.password;
-    }
-    // insert database call here
-    if (users[0].name !== name) {
-        res.status(401).json({message:"no such user found"});
-        return;
-    }
-
-    // replace with crypto algorithm later
-    if (users[0].password === req.body.password) {
-        var payload = {id: users[0].id};
-        var token = jwt.sign(payload, process.env.SECRET);
-        res.json({message: "ok", token: token});
+    
+        users.findUser(name, function(err, user) {
+            if (err || !user) {
+                res.status(401).json({message:"no such user found"});
+                return;
+            }
+            verify(user, password, res);
+        });
     } else {
-        res.status(401).json({message:"passwords did not match"});
+        res.status(401).json({message:"missing username or password"});
     }
 });
+
+function verify(user, password, res) {
+    bcrypt.compare(password, user.pass, function(err, comparison) {
+        if (err) console.log(err);
+        if (comparison) {
+            var payload = {id: user.id};
+            var token = jwt.sign(payload, process.env.SECRET);
+            res.json({message: "ok", token: token});
+        } else {
+            res.status(401).json({message:"passwords did not match"});
+        }
+    });
+}
 
 module.exports = router;
