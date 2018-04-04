8d969dc5888661d238b1730b0874e0dedd4e34ac
Dennis Tran <nartdensin@gmail.com>
2018-04-04 00:15:25 -0700

    add CORS middleware

--- app.js
+++ app.js
@@ -37,6 +37,35 @@ var passportStrategy = require('./helper/passport').strategy;
 passport.use(passportStrategy);
 app.use(passport.initialize());
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  var oneof = false;
+  if(req.headers.origin) {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      oneof = true;
+  }
+  if(req.headers['access-control-request-method']) {
+      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+      oneof = true;
+  }
+  if(req.headers['access-control-request-headers']) {
+      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+      oneof = true;
+  }
+  if(oneof) {
+      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+  }
+
+  // intercept OPTIONS method
+  if (oneof && req.method == 'OPTIONS') {
+      res.send(200);
+  }
+  else {
+      next();
+  }
+}
+app.use(allowCrossDomain);
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/search', search);
